bbd40eac13889a1a7da4c8e97a55e7f223c94e3b
Leonardo Rico <104222321+LeonardoRico@users.noreply.github.com>
2024-08-22 09:39:00 -0500

    Commit with Intent component

--- app/src/main/java/com/example/clase2/MainActivity.java
+++ app/src/main/java/com/example/clase2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.clase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,28 +17,39 @@ public class MainActivity extends AppCompatActivity {
     Button hola,mundo;
 
     String nombreUsuario;
+    String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG,"Estoy en el onCreate");
 
         ingresarUsuario = (EditText) findViewById(R.id.editarUsuario);
         usuario = (TextView) findViewById(R.id.txvUsuario);
         hola = findViewById(R.id.btnHola);
         mundo = findViewById(R.id.btnMundo);
 
-
         hola.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "estoy en el botón");
 
-                usuario.setText("Presionaste el botón Hola");
-                nombreUsuario= String.valueOf(ingresarUsuario.getText());
+                Intent pasarDatos = new Intent(MainActivity.this, segunda_pantalla.class);
+                pasarDatos.putExtra("Nombre", "Juan");
+                pasarDatos.putExtra("Edad", 20);
+                startActivity(pasarDatos);
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"Estoy en el onStart");
+
 
         mundo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +57,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG,"Estoy en el onResume");
+        //poner mensaje en pantalla
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"Estoy en el onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"Estoy en el onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"Estoy en el onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"Estoy en el onDestroy");
     }
 }
